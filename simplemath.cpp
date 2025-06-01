5cf85e05cbc442d0508877d30c33e67a63edc3be
Seito Yokota <seitoyokota15@icloud.com>
2025-06-01 09:03:14 -0500

    math

---
+++ simplemath.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter a numbers: ";
+    cin>>a;
+    cout<<"enter another number: ";
+    cin>>b;
+    c=a+b;
+    cout<<a<<" + "<<b<<" = "<<c<<endl;
+}
