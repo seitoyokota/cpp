50a84b48df52d8deb2063f7ed428b2502d7e71f6
Seito Yokota <seitoyokota15@icloud.com>
2026-01-03 22:29:46 -0600

    database code

---
+++ database.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+    vector<int> data_int;
+    vector<char> data_char;
+    vector<string> data_string;
+    vector<float> data_float;
+    vector<double> data_double;
+    cout<<"enter the title of this data base: ";
+    cin>>name;
+
+}
